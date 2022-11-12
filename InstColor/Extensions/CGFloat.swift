5be0ee2f2588e3ed4b734317ed3fa83f82aed50c
stevencao59 <steven.cao.rider@gmail.com>
2022-11-11 21:40:50 -0500

    - Added color detail view - Added all backend functions to calculate colors

---
+++ InstColor/Extensions/CGFloat.swift
@@ -0,0 +1,14 @@
+//
+//  CGFloat.swift
+//  InstColor
+//
+//  Created by Lei Cao on 11/11/22.
+//
+
+import Foundation
+
+extension CGFloat {
+    func getMod() {
+        
+    }
+}
--- InstColor/Extensions/View.swift
+++ InstColor/Extensions/View.swift
@@ -2,15 +2,13 @@
 //  View.swift
 //  InstColor
 //
-//  Created by Lei Cao on 10/13/22.
+//  Created by Lei Cao on 11/8/22.
 //
 
-import Foundation
 import SwiftUI
 
 extension View {
-    func measureSize(perform action: @escaping (CGSize) -> Void) -> some View {
-        self.modifier(MeasureSizeModifier())
-            .onPreferenceChange(SizePreferenceKey.self, perform: action)
+    func clearModalBackground() -> some View {
+        self.modifier(ClearBackgroundViewModifier())
     }
 }
